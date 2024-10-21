7cb2a8f71d426a96332178e4c49266da2987df7c
Edi Buhic <edi@buhic.se>
2024-10-21 18:15:40 +0200

    Lab 8

--- lab8/src/lab8/P8_2.java
+++ lab8/src/lab8/P8_2.java
@@ -1,6 +1,7 @@
 package lab8;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -9,46 +10,54 @@ public class P8_2 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		
+
 		System.out.printf("Ange filnamnet på filen som ska avläsas: ", "%n");
 		String fileData = readFile(s.nextLine());
-		
+
 		System.out.print("Ange nya filens namn: ");
 		writeFile(fileData, s.nextLine());
-		
+
 		s.close();
+		System.exit(0);
 	}
-	
+
 	public static String readFile(String fileName) {
-		Scanner s = new Scanner(fileName);
-		String fileData = "";
-		
-		while (s.hasNext()) {
-			fileData += s.nextLine();
+		File openFile = new File(fileName);
+		String fileData = null;
+
+		try {
+			Scanner s = new Scanner(openFile);
+
+			while (s.hasNext()) {
+				fileData += s.nextLine() + "\n";
+			}
+			s.close();
+		} catch (FileNotFoundException err) {
+			System.exit(1);
 		}
-		s.close();
+
 		return fileData;
 	}
-	
+
 	public static void writeFile(String data, String newFileName) {
 		File newFile = new File(newFileName);
 		Scanner s = new Scanner(data);
 		int rowCount = 1;
-		
+
 		try {
 			FileWriter fileWrite = new FileWriter(newFile);
-			
+
 			while (s.hasNext()) {
-				fileWrite.write("/* " + rowCount + " */ " + s.nextLine());
+				fileWrite.write("/* " + rowCount + " */ " + s.nextLine() + "\n");
 				rowCount++;
 			}
-			
+
 			s.close();
 			fileWrite.close();
 		} catch (IOException e) {
 			System.exit(1);
 		}
-			
-		System.out.println("Kopierade " + rowCount + " rader till " + newFileName + ".");
+
+		System.out.println("Kopierade " + (rowCount - 1) + " rader till " + newFileName + ".");
 	}
 }
