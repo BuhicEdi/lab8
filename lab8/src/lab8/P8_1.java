7c1720fa531085d76a06ffc41b52d47a517c22e7
Edi Buhic <edi@buhic.se>
2024-10-21 14:39:04 +0200

    Lab 8

--- lab8/src/lab8/P8_1.java
+++ lab8/src/lab8/P8_1.java
@@ -1,5 +1,21 @@
 package lab8;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class P8_1 {
 
+	public static void main(String[] args) {
+		File hello = new File("hello.txt");
+		
+		try {
+		FileWriter helloWrite = new FileWriter(hello);
+		helloWrite.write("pucksac");
+		helloWrite.close();
+		} catch (IOException err) {
+			System.out.println(err.getLocalizedMessage());
+		}
+	System.exit(0);	
+	}
 }
