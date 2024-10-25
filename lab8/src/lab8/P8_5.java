ec06bc16aad54efbde000ad28fb690e50fbb306e
Edi Buhic <edi@buhic.se>
2024-10-25 12:12:40 +0200

    Lab 8

--- lab8/src/lab8/P8_5.java
+++ lab8/src/lab8/P8_5.java
@@ -8,6 +8,7 @@ public class P8_5 {
 
 	public static void main(String[] args) {
 		Scanner fileScan = new Scanner(System.in);
+		int lines = 0, words = 0, chars = 0;
 
 		System.out.printf("Mata in ett filnamn: ", "%n");
 		String fileName = fileScan.next();
@@ -16,35 +17,29 @@ public class P8_5 {
 		try {
 			File fileImport = new File(fileName);
 			Scanner fileScanner = new Scanner(fileImport);
-			getFileStats(fileScanner);
+
+			while (fileScanner.hasNextLine()) {
+				String currentLine = fileScanner.nextLine();
+				System.out.println(currentLine);
+				lines++;
+				chars += currentLine.length();
+
+				Scanner wordScan = new Scanner(currentLine);
+				while (wordScan.hasNext()) {
+					wordScan.next();
+					words++;
+				}
+				wordScan.close();
+			}
+			fileScanner.close();
+
+			System.out.println("Antalet rader var: " + lines);
+			System.out.println("Antalet ord var: " + words);
+			System.out.println("Antalet tecken var: " + chars);
 
 			System.exit(0);
 		} catch (FileNotFoundException err) {
 			err.printStackTrace();
 		}
 	}
-
-	private static void getFileStats(Scanner fileScanner) throws FileNotFoundException {
-		int lines = 0, words = 0, chars = 0;
-
-		while (fileScanner.hasNextLine()) {
-			String currentLine = fileScanner.nextLine();
-			System.out.println(currentLine);
-			lines++;
-			chars += currentLine.length();
-
-			Scanner wordScan = new Scanner(currentLine);
-			while (wordScan.hasNext()) {
-				wordScan.next();
-				words++;
-			}
-			wordScan.close();
-		}
-		fileScanner.close();
-
-		System.out.println("Antalet rader var: " + lines);
-		System.out.println("Antalet ord var: " + words);
-		System.out.println("Antalet tecken var: " + chars);
-	}
-
 }
