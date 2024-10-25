db615121c45dcb5b993be2ec82f0d1947676cca1
Edi Buhic <edi@buhic.se>
2024-10-25 11:31:43 +0200

    Lab 8

--- lab8/src/lab8/P8_5.java
+++ lab8/src/lab8/P8_5.java
@@ -1,6 +1,8 @@
 package lab8;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class P8_5 {
@@ -11,27 +13,38 @@ public class P8_5 {
 		System.out.printf("Mata in ett filnamn: ", "%n");
 		String fileName = fileScan.next();
 		fileScan.close();
-		
-		getFileStats(fileName);
+
+		File fileImport = new File(fileName);
+
+		try {
+			getFileStats(fileImport);
+			System.exit(0);
+		} catch (IOException err) {
+			System.exit(1);
+		}
+
 	}
-	
-	public static void getFileStats(String fileName) {
-		Scanner lineScan = new Scanner(fileName);
+
+	public static void getFileStats(File file) throws IOException {
+
+		Scanner lineScan = new Scanner(file);
 		int lines = 0, words = 0, chars = 0;
-		
+
 		while (lineScan.hasNextLine()) {
 			String currentLine = lineScan.nextLine();
+			System.out.println(currentLine);
 			lines++;
 			chars += currentLine.length();
-			
+
 			Scanner wordScan = new Scanner(currentLine);
 			while (wordScan.hasNext()) {
+				wordScan.next();
 				words++;
 			}
 			wordScan.close();
 		}
 		lineScan.close();
-		
+
 		System.out.println("Antalet rader var: " + lines);
 		System.out.println("Antalet ord var: " + words);
 		System.out.println("Antalet tecken var: " + chars);
